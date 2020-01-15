33098b5db9868ecd76c2f5bcd541885256a3dd1f
AmyBraswell <braswellamy@mac.com>
2020-01-15 01:52:21 -0600

    all API calls working / ready to submit MVP

--- index.js
+++ index.js
@@ -1,123 +1,39 @@
-'use strict';
+"use strict";
+
+$(watchUserForm);
 
 //////////////////API Keys/////////////////
 
 const googleGeocodingAPI = "AIzaSyBF855waFb_88pD3B4BJB42JNuvSk05x34";
 
 const AccuWeatherAPI = "pU3saOZ250CYJ2bcc3CCzkQZSYTn7xO9";
-const openWeatherAPI = "a6015cfb7b1a11caec9fbb1bb5150329";
 const weatherBitAPI = "0a906bc695244645a034fa4c4baf11e3";
 const darkSkyAPI = "05cb46aa19287b7b9c1a235035e4c45f";
  
-
 //////////////////Base URLs///////////////
 
 const googleGeocodingBase ="https://maps.googleapis.com/maps/api/geocode/json?address=";
 
-const openWeatherBase = "http://api.openweathermap.org/data/2.5/weather?";
+const Proxy = "https://cors-anywhere.herokuapp.com/";
+
 const weatherBitBase = "https://api.weatherbit.io/v2.0/current?";
 const darkSkyBase = "https://api.darksky.net/forecast/";
-//const accuWeatherBaseLocation = "http://dataservice.accuweather.com/locations/v1/postalcodes/search.json?q=60607,us&apikey=pU3saOZ250CYJ2bcc3CCzkQZSYTn7xO9";
-         //above to get Location_Key  then:
-//const accuWeatherBase = "http://dataservice.accuweather.com/currentconditions/v1/" + Location_Key;
+const accuWeatherBaseLocation = "http://dataservice.accuweather.com/locations/v1/postalcodes/search.json?q=";
+const accuWeatherBase = "http://dataservice.accuweather.com/currentconditions/v1/";
 
-//const accuWeatherWorkingURL = "http://dataservice.accuweather.com/currentconditions/v1/26463_PC?apikey=pU3saOZ250CYJ2bcc3CCzkQZSYTn7xO9&language=en-us&details=false HTTP/1.1";
+//////////////// Watch Form and Make API Calls ////////////////
 
-
-
-
- //////////////// Render Weather Report ////////////////
- /*
- function renderResult(latitude, longitude) {
-  $('#results-list').append(
-    `
-      <div class="returned-park">
-          <h3>${latitude}, ${longitude}</br></br> 
-          <div class="text">
-              <p>${latitude}, ${longitude}</br></p>
-          </div>
-      </div> 
-    `
-  );
-}
-*/
-
-
-$(document).ready(function() { 
-
-  var proxy = 'https://cors-anywhere.herokuapp.com/';
-  var apiLinkDS = "https://api.darksky.net/forecast/05cb46aa19287b7b9c1a235035e4c45f/38,-122";
-
-  $.ajax({
-    url: proxy + apiLinkDS,
-    success:function(data) { console.log(data);}
-  });
-
-}); 
-
-$(document).ready(function() { 
-
-  var proxy = 'https://cors-anywhere.herokuapp.com/';
-  var apiLinkDS = "http://dataservice.accuweather.com/currentconditions/v1/26463_PC?apikey=pU3saOZ250CYJ2bcc3CCzkQZSYTn7xO9&language=en-us&details=false HTTP/1.1";
-
-  $.ajax({
-    url: proxy + apiLinkDS,
-    success:function(data) { console.log(data);}
-  });
-
-}); 
-
-
-
-
-//////////////// Watch User Form //////////////////////
-/*
 function watchUserForm() {
-    $('form').submit(event => {
-        event.preventDefault(); 
-        const fromZipInput = $('#js-zip-code').val();
-        const fromCountryInput = $('#js-country-code').val();
-        getLatLngByZipcode(fromZipInput);
 
-        /////////// Get Lat / Long From Zip //////////
+  $("form").submit(event => {
+    event.preventDefault();
+    const fromZipInput = $("#js-zip-code").val();
+    getLatLngByZipcode(fromZipInput);
 
-        function getLatLngByZipcode() 
-        {
-            const googleGeocoder = googleGeocodingBase  + fromZipInput +  "&key=" + googleGeocodingAPI;
+    function getLatLngByZipcode(latitude, longitude) {
+        const googleGeocoder = googleGeocodingBase  + fromZipInput +  "&key=" + googleGeocodingAPI;
 
-            fetch(googleGeocoder)
-            .then(response => {
-              if (response.ok) {
-              return response.json();
-              }
-              throw new Error(response.statusText);
-            })
-            .then((myJson => {
-              constructURL(myJson.results[0].geometry.location.lat, myJson.results[0].geometry.location.lng);
-              console.log("Google API call to: " + googleGeocoder);
-              //console.log(myJson.results[0]);
-              let latitude = myJson.results[0].geometry.location.lat;
-              let longitude = myJson.results[0].geometry.location.lng;
-              console.log ("inside Fetch call: " + latitude + ", " + longitude);
-            }))
-            .catch(err => {
-              $('#js-error-message').text(`Something went wrong: ${err.message}`);
-              }
-            );
-        }
-        
-        function constructURL(latitude, longitude) {
-          const darkSkyURL = darkSkyBase + darkSkyAPI + "/" + latitude + "," + longitude;
-          console.log(darkSkyURL);
-
-
-
-
-
-        
-        //pass weather api url into fetch command
-        fetch(darkSkyURL, {
-            mode: 'no-cors'})
+        fetch(googleGeocoder)
         .then(response => {
           if (response.ok) {
           return response.json();
@@ -125,23 +41,122 @@ function watchUserForm() {
           throw new Error(response.statusText);
         })
         .then((myJson => {
-          //renderResult(myJson);
-          console.log(myJson);
+          catCall(myJson.results[0].geometry.location.lat, myJson.results[0].geometry.location.lng);
+          let latitude = myJson.results[0].geometry.location.lat;
+          let longitude = myJson.results[0].geometry.location.lng;
         }))
         .catch(err => {
-          $('#js-error-message').text(`Something went wrong: ${err.message}`);
+          $("#js-error-message").text(`Something went wrong: ${err.message}`);
+          }
+        );
+
+        function catCall(latitude, longitude) {
+          const darkSkyURL = darkSkyBase + darkSkyAPI + "/" + latitude + "," + longitude;
+
+          //////////////////////// Call 1st Weather API ///////////////////////
+          ///////// AJAX Call to Proxy as Work Around for CORS Policy /////////
+          let catApiLinkDS = darkSkyURL;
+        
+          $.ajax({
+            url: Proxy + catApiLinkDS,
+            success:function(data) { 
+              let catSummary = "The current condition is " + data.currently.summary.toLowerCase() + ".";
+              let catTemp = "The current temperature is " + data.currently.temperature + " degrees F."; 
+              let catApparent = "It feels like " + data.currently.apparentTemperature + " degrees F.";
+              renderCatResult(catSummary, catTemp, catApparent);
+            }
+          });
+        }
+    } 
+
+          /////////////////// Call 2nd Weather API (Dog) //////////////////////
+          ///// AJAX Call to Proxy as Work Around for HTTP / GitHub Issue /////
+          let dogProxy = "https://cors-anywhere.herokuapp.com/";
+          let dogApiLocation = accuWeatherBaseLocation + fromZipInput + ",us&apikey=" + AccuWeatherAPI;
+
+          $.ajax({
+            url: Proxy + dogApiLocation,
+            success:function(data) { 
+              let dogLocation = data[0].Key;
+              let dogApiLinkDS = accuWeatherBase + dogLocation + "?apikey=" + AccuWeatherAPI + "&language=en-us&details=false HTTP/1.1";
+              secondDogCall(dogApiLinkDS);
+            }
+          });
+          
+          function secondDogCall(dogApiLinkDS) {
+            $.ajax({
+              url: Proxy + dogApiLinkDS,
+              success:function(data) { 
+                let dogSummary = "The current condition is " + data[0].WeatherText.toLowerCase() + ".";
+                let dogTemp = "The current temperature is " + data[0].Temperature.Imperial.Value;
+                let dogTempUnit = data[0].Temperature.Imperial.Unit + ".";
+                renderDogResult(dogSummary, dogTemp, dogTempUnit);
+              }
+          });
+        }
+        
+          ///////////// Call 3rd Weather API (Bird) ////////////////
+          let weatherBitURL = weatherBitBase + "postal_code=" + fromZipInput + "&country=US&units=I&key=" + weatherBitAPI; 
+          
+          fetch(weatherBitURL)
+          .then(response => {
+            if (response.ok) {
+            return response.json();
+            }
+            throw new Error(response.statusText);
+          })
+          .then((myJson => {
+            let birdSummary = "The current condition is " + myJson.data["0"].weather.description.toLowerCase() + ".";
+            let birdTemp = "The current temperature is " + myJson.data["0"].temp + " degrees F.";
+            let birdApparent = "It feels like " + myJson.data["0"].app_temp + " degrees F.";
+            renderbirdResult(birdSummary, birdTemp, birdApparent);
+          }))
+          .catch(err => {
+            $("#js-error-message").text(`Something went wrong: ${err.message}`);
             }
           ); 
-        } 
-
-
-      });
+  });
+}
+ 
+//////////////// Render Weather Report ////////////////
+ 
+function renderCatResult(catSummary, catTemp, catApparent) {
+  $("#js-cat-results").prepend(
+    `
+      <div class="js-dark-sky">
+        <h2>The cat says:</h2>
+        <p>${catSummary}</p>
+        <p>${catTemp}</p>
+        <p>${catApparent}</p>
+      </div> 
+    `
+  );
 }
 
+function renderDogResult(dogSummary, dogTemp, dogTempUnit) {
+  $("#js-dog-results").append(
+    `
+      <div class="js-accuweather">
+        <h2>The dog says:</h2>
+        <p>${dogSummary}</p>
+        <p>${dogTemp} ${dogTempUnit}</p>
+      </div> 
+    `
+  );
+}
 
-    $(watchUserForm);
-
-*/
+function renderbirdResult(birdSummary, birdTemp, birdApparent) {
+  $("#js-bird-results").append(
+    `
+      <div class="js-weatherbit">
+        <h2>The bird says:</h2>
+        <p>${birdSummary}</p>
+        <p>${birdTemp}</p>
+        <p>${birdApparent}</p>
+      </div> 
+    `
+  );
+}
 
 
 
